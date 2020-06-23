49093db2f0fbabc846a26905973611376ee40b96
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 08:36:47 +0300

    Initial Commit

---
+++ WhatsNewInSwiftUIiOS14/WhatsNewInSwiftUIiOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNewInSwiftUIiOS14App.swift
+//  WhatsNewInSwiftUIiOS14
+//
+//  Created by Ramill Ibragimov on 23.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct WhatsNewInSwiftUIiOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
