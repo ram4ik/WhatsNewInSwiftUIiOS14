bd5b876b09c975afc76cf440821ace9d06b22ee6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-25 16:35:17 +0300

    colorPicker

--- WhatsNewInSwiftUIiOS14/ContentView.swift
+++ WhatsNewInSwiftUIiOS14/ContentView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), latitudinalMeters: 0.5, longitudinalMeters: 0.5)
+    @State private var color = Color.white
     
     var body: some View {
-        Map(coordinateRegion: $region)
+        ColorPicker("Pick a color", selection: $color)
     }
 }
 
