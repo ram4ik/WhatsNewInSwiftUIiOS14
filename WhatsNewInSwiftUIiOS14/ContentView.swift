6fce3c1402dae44625f69f9813c07920e3a4f44e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-25 16:26:31 +0300

    no self

--- WhatsNewInSwiftUIiOS14/ContentView.swift
+++ WhatsNewInSwiftUIiOS14/ContentView.swift
@@ -11,11 +11,11 @@ struct ContentView: View {
     @State private var downloadAmount = 0.0
     
     var body: some View {
-        VStack {
-            ProgressView("Loading...", value: downloadAmount, total: 100)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Button("Move along please") {
-                downloadAmount += 5
+        ScrollView {
+            VStack {
+                ForEach(0...1000, id: \.self) { i in
+                    Text("Row \(i)")
+                }
             }
         }
     }
